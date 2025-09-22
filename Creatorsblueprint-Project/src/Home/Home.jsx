2c531db9beac1feb1c5cb5cbf980bbbda61ee748
LebohangDev <phxsoul39@gmail.com>
2025-09-22 16:47:45 +0400

    payment plan 1 complete

--- Creatorsblueprint-Project/src/Home/Home.jsx
+++ Creatorsblueprint-Project/src/Home/Home.jsx
@@ -67,6 +67,93 @@ function Home(){
                 </div>
             </div>
 
+            <div className={styles.row2}>
+
+                <div className={styles.title}>
+                    <h1>Your Plan, Done-for-You</h1>
+                    <p>Manage, track, and optimize your digital assets with a plan built for your needs.</p>
+                </div>
+
+                <div className={styles.paymentPlanContainer}>
+                    <div className={styles.header}>
+                        <div className={styles.title2}>
+                            <h1>Done-For-You</h1>
+                            <p>Your full system to grow, scale, and earn more — faster.</p>
+
+                        </div>
+
+                        <div className={styles.highlight}>
+                            <p>Best Choice</p>
+
+                        </div>
+                        
+                        
+                    </div>
+
+                    <div className={styles.desc}>
+                        <div className={styles.title3}>
+                            <h1>What's Included:</h1>
+                        </div>
+
+                        <div className={styles.content}>
+                            <div className={styles.item}>
+                                <i className="ri-check-line"></i>
+                                <p>Premium, Detailed Website (Multiple Tabs, Unlimited Edits)</p>
+                            </div>
+                            
+                            <div className={styles.item}>
+                                <i className="ri-check-line"></i>
+                                <p>Payment Gateway Setup (PayPal / Stripe)</p>
+                            </div>
+                            <div className={styles.item}>
+                                <i className="ri-check-line"></i>
+                                <p>Unlimited Digital Products (Ebooks, Templates, Courses)</p>
+                            </div>
+                            <div className={styles.item}>
+                                <i className="ri-check-line"></i>
+                                <p>Complete Monetization Strategy</p>
+                            </div>
+                            <div className={styles.item}>
+                                <i className="ri-check-line"></i>
+                                <p>Monthly 1-on-1 Strategy Calls</p>
+                            </div>
+                            <div className={styles.item}>
+                                <i className="ri-check-line"></i>
+                                <p>Ongoing Launch Support</p>
+                            </div>
+                            <div className={styles.item}>
+                                <i className="ri-check-line"></i>
+                                <p>Domain Included (Setup + Connection)</p>
+                            </div>
+                            
+                        </div>
+
+                        
+                    </div>
+                    <div className={styles.perfect}>
+                        <p>Perfect for: Creators who want to scale their income with a complete, professional system.</p>
+                    </div>
+                    <div className={styles.priceContainer}>
+                        <div className={styles.cost}>
+                            <h1>$5.99</h1>
+                            <p>/per month</p>
+
+                        </div>
+
+                        <div className={styles.billed}>
+                            <p>billeld yearly</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.paymentButton}>
+                        <button>Upgrade</button>
+                    </div>
+
+
+                </div>
+
+            </div>
+
         </div>
         
         </>
