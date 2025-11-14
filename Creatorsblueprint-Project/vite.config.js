7ebe97e4d88f70409ea972274630303637935586
LebohangDev <phxsoul39@gmail.com>
2025-11-14 18:18:48 +0400

    changign base to empty serve at orgin

--- Creatorsblueprint-Project/vite.config.js
+++ Creatorsblueprint-Project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Creatorsblueprint/test/',
+  base: '/',
   plugins: [react()],
   server:{
 
