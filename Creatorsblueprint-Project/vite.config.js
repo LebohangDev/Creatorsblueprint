2bab04fe4cd1fb13e7b95f926a11c05988d29d4b
LebohangDev <phxsoul39@gmail.com>
2025-11-14 18:46:09 +0400

    changing base

--- Creatorsblueprint-Project/vite.config.js
+++ Creatorsblueprint-Project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Creatorsblueprint/Creatorsblueprint-Project/test/',
+  base: '/test/',
   plugins: [react()],
   server:{
     
